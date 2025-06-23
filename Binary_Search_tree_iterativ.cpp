f80adfbc2b6f3c8f8f9e5cc7a1381a957668b3a9
Davo <dkirakosyan17@gmail.com>
2025-06-23 19:12:50 +0400

    Binary Search Tree

--- Binary_Search_tree_iterativ.cpp
+++ Binary_Search_tree_iterativ.cpp
@@ -1,1 +1,149 @@
-#
+#include <iostream>
+#include <stack>
+using namespace std;
+
+template <typename T>
+class BST {
+    struct TreeNode {
+        T value;
+        TreeNode* left;
+        TreeNode* right;
+
+        TreeNode(T val) : value{val}, left{nullptr}, right{nullptr} {}
+        TreeNode(T val, TreeNode* lefts, TreeNode* rights) : value{val}, left{lefts}, right{rights} {}
+    };
+
+    TreeNode* root;
+
+    void preorder_traversal(TreeNode* node) {
+        if (node == nullptr) return;
+
+        stack<TreeNode*> stas;
+        stas.push(node);
+
+        while (!stas.empty()) {
+            TreeNode* curr = stas.top();
+            stas.pop();
+            cout << curr->value << " ";
+            if (curr->right) stas.push(curr->right);
+            if (curr->left) stas.push(curr->left);
+        }
+    }
+
+    TreeNode* find_predecessor(TreeNode* node) {
+        if (node == nullptr) return nullptr;
+        while (!(node->left)) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    TreeNode* find_successor(TreeNode* node) {
+        if (node == nullptr) return nullptr;
+        while (node->left) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    TreeNode* helper_insert(TreeNode* root, T values) {
+        if (root == nullptr) {
+            return new TreeNode(values);
+        }
+
+        TreeNode* node = root;
+        TreeNode* parent = nullptr;
+
+        while (node) {
+            parent = node;
+            if (node->value > values) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+        }
+
+        if (values > parent->value) {
+            parent->right = new TreeNode(values);
+        } else {
+            parent->left = new TreeNode(values);
+        }
+
+        return root;
+    }
+
+    TreeNode* helper_deletion(T values) {
+        TreeNode* current = root;
+        TreeNode* parent = nullptr;
+        int flag = 1;
+
+        while (current && current->value != values) {
+            parent = current;
+            if (current->value > values) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+
+        if (parent == nullptr) {
+            flag = 0;
+        }
+
+        if (current->left == nullptr) {
+            parent = current->right;
+            delete current;
+            if (flag == 0) return parent;
+        }
+
+        if (current->right == nullptr) {
+            parent = current->left;
+            delete current;
+            if (flag == 0) return parent;
+        }
+
+        TreeNode* ptr = find_successor(current->right);
+        current->value = ptr->value;
+
+        if (values > current->value) {
+            current->right = ptr->right;
+        } else {
+            current->left = ptr->right;
+        }
+
+        delete ptr;
+
+        if (flag == 0) return current;
+        return root;
+    }
+
+public:
+    BST() : root{nullptr} {}
+
+    void print() {
+        preorder_traversal(root);
+        cout << endl;
+    }
+
+    void insert(T values) {
+        root = helper_insert(root, values);
+    }
+
+    bool find(T values) {
+        TreeNode* current = root;
+        while (current) {
+            if (current->value == values) return true;
+            if (current->value > values) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        return false;
+    }
+
+    TreeNode* deletion(T value) {
+        root = helper_deletion(value);
+        return root;
+    }
+};
--- Binary_Search_tree_recurive.cpp
+++ Binary_Search_tree_recurive.cpp
@@ -1,127 +1,121 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 template <typename T>
-class BST{
-struct TreeNode{
-  T value;
-  TreeNode* left;
-  TreeNode* right;
-  TreeNode(T val):value{val},left{nullptr},right{nullptr}{};
-  TreeNode(T val,TreeNode* lefts,TreeNode* rights):value{val},left{lefts},right{rights}{};
-};
-TreeNode* root;
-void Inorder_traversal(TreeNode* node){
-   if(node==nullptr){
-    return;
-   }
-   Inorder_traversal(node->left);
-   cout<<node->value<<" ";
-   Inorder_traversal(node->right);
-}
-void Preorder_traversal(TreeNode* node){
-  if(node==nullptr){
-    return;
-   }
-   cout<<node->value<<" ";
-   Preorder_traversal(node->left);
-   Preorder_traversal(node->right);
-}
-void Postorder_traversal(TreeNode* node){
-   if(node==nullptr){
-    return;
-   }
-   Postorder_traversal(node->left);
-   Postorder_traversal(node->right);
-   cout<<node->value<<" ";
-}
-TreeNode* helper_insert(TreeNode* node, T values){
-   if(node==nullptr){
-        node=new TreeNode(values);
+class BST {
+    struct TreeNode {
+        T value;
+        TreeNode* left;
+        TreeNode* right;
+
+        TreeNode(T val) : value{val}, left{nullptr}, right{nullptr} {}
+        TreeNode(T val, TreeNode* lefts, TreeNode* rights) : value{val}, left{lefts}, right{rights} {}
+    };
+
+    TreeNode* root;
+
+    void inorder_traversal(TreeNode* node) {
+        if (node == nullptr) return;
+        inorder_traversal(node->left);
+        cout << node->value << " ";
+        inorder_traversal(node->right);
+    }
+
+    void preorder_traversal(TreeNode* node) {
+        if (node == nullptr) return;
+        cout << node->value << " ";
+        preorder_traversal(node->left);
+        preorder_traversal(node->right);
+    }
+
+    void postorder_traversal(TreeNode* node) {
+        if (node == nullptr) return;
+        postorder_traversal(node->left);
+        postorder_traversal(node->right);
+        cout << node->value << " ";
+    }
+
+    TreeNode* helper_insert(TreeNode* node, T values) {
+        if (node == nullptr) {
+            node = new TreeNode(values);
+            return node;
+        }
+        if (node->value > values) {
+            node->left = helper_insert(node->left, values);
+        } else {
+            node->right = helper_insert(node->right, values);
+        }
         return node;
-   }
-  if(node->value > values){
-    node->left=helper_insert(node->left,values);
-  }
-  else{
-      node->right=helper_insert(node->right,values);
-  }
-  return node;
-}
-bool find_helper(TreeNode* node,T values){
-  if(node==nullptr){
-    return false;
-  }
-  if(node->value==values){
-    return true;
-  }
-  if(node->value>values){
-    return find_helper(node->left,values);
-  }
-  else{
-    return find_helper(node->right,values);
-  }
-}
-TreeNode* find_successor(TreeNode* node){
-  if(node->left==nullptr){
-    return node;
-  }
-  return find_successor(node->left);
-}
-TreeNode* find_predecessor(TreeNode* node){
-  if(node->right==nullptr){
-    return node;
-  }
-  return find_predecessor(node->right);
-}
-TreeNode* helper_deletion(TreeNode* node,T values){
-    if(node==nullptr){
-         return node;
-   }
-   if(node->value<values){
-       node->right=helper_deletion(node->right,values);
-   }
-   if(node->value>values){
-    node->left=helper_deletion(node->left,values);
-   }
-   if(node->value==values){
-    if(node->left==nullptr){
-       TreeNode* temp=node->right;
-        delete node;
-        return temp;
     }
-    if(node->right==nullptr){
-      TreeNode*temp=node->left;
-      delete node;
-      return temp;
-    }
-    TreeNode*ptr=find_successor(node->right);
-    node->value=ptr->value;
-    node->right=helper_deletion(node->right,ptr->value);
 
-   }
-   return node;
-}
+    bool find_helper(TreeNode* node, T values) {
+        if (node == nullptr) return false;
+        if (node->value == values) return true;
+        if (node->value > values) return find_helper(node->left, values);
+        return find_helper(node->right, values);
+    }
+
+    TreeNode* find_successor(TreeNode* node) {
+        if (node->left == nullptr) return node;
+        return find_successor(node->left);
+    }
+
+    TreeNode* find_predecessor(TreeNode* node) {
+        if (node->right == nullptr) return node;
+        return find_predecessor(node->right);
+    }
+
+    TreeNode* helper_deletion(TreeNode* node, T values) {
+        if (node == nullptr) return node;
+
+        if (node->value < values) {
+            node->right = helper_deletion(node->right, values);
+        } else if (node->value > values) {
+            node->left = helper_deletion(node->left, values);
+        } else {
+            if (node->left == nullptr) {
+                TreeNode* temp = node->right;
+                delete node;
+                return temp;
+            }
+            if (node->right == nullptr) {
+                TreeNode* temp = node->left;
+                delete node;
+                return temp;
+            }
+            TreeNode* ptr = find_successor(node->right);
+            node->value = ptr->value;
+            node->right = helper_deletion(node->right, ptr->value);
+        }
+
+        return node;
+    }
+
 public:
-BST(){root=nullptr;}
- void insert( T values){
-     root=helper_insert(root,values);
-}
-bool find(T values){
-    return find_helper(root,values);
-}
-    TreeNode* deletion( T values){
-   root=helper_deletion(root,values);
-   return root;
-}
-void print_inorder(){
-  Inorder_traversal(root);
-}
-void print_preorder(){
-  Preorder_traversal(root);
-}
-void print_postorder(){
-  Postorder_traversal(root);
-}
-};
+    BST() : root{nullptr} {}
 
-    
+    void insert(T values) {
+        root = helper_insert(root, values);
+    }
+
+    bool find(T values) {
+        return find_helper(root, values);
+    }
+
+    TreeNode* deletion(T values) {
+        root = helper_deletion(root, values);
+        return root;
+    }
+
+    void print_inorder() {
+        inorder_traversal(root);
+    }
+
+    void print_preorder() {
+        preorder_traversal(root);
+    }
+
+    void print_postorder() {
+        postorder_traversal(root);
+    }
+};
